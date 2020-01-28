fdb4e7a66c9428ca4900202e54b7c1ba98166654
Wanthanai <pentorework@gmail.com>
2020-01-28 23:52:39 +0700

    [Edit] Cookie-Session

--- Server/config/keys.js
+++ Server/config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
   googleClientID: '131443872426-nlrpf2c2ms9s9pn86tcpgqkrrlcg1o8h.apps.googleusercontent.com',
   googleClientSecret: '9TGTvQcG7qTrt-4yz43jZOtm',
-  mongoURI: 'mongodb+srv://emailyuser:vf5NeKveqVw6Begk@emaily-xdkcm.mongodb.net/test?retryWrites=true&w=majority'
+  mongoURI: 'mongodb+srv://emailyuser:vf5NeKveqVw6Begk@emaily-xdkcm.mongodb.net/test?retryWrites=true&w=majority',
+  cookieKey: 'qsefthukoplijygrdwazsxdcfvgbhnjmk'
 };
