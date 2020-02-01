24f16958b5c5a86950ea48c598c84171dbaf066b
Wanthanai <pentorework@gmail.com>
2020-02-01 23:50:30 +0700

    [Finish] Auth Flow

--- Server/config/keys.js
+++ Server/config/keys.js
@@ -1,6 +1,8 @@
-module.exports = {
-  googleClientID: '131443872426-nlrpf2c2ms9s9pn86tcpgqkrrlcg1o8h.apps.googleusercontent.com',
-  googleClientSecret: '9TGTvQcG7qTrt-4yz43jZOtm',
-  mongoURI: 'mongodb+srv://emailyuser:vf5NeKveqVw6Begk@emaily-xdkcm.mongodb.net/test?retryWrites=true&w=majority',
-  cookieKey: 'qsefthukoplijygrdwazsxdcfvgbhnjmk',
-};
+// keys.js - figure out what set of credentials to retuen
+if (process.env.NODE_ENV === 'production') {
+  //we are in production - return the prod set of keys.
+  module.exports = require('./prod')
+} else {
+  //we are in development - return the dev keys!!
+  module.exports = require('./dev');
+}
