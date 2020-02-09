1e43d0c7635f6aeef1ae73943c2fcc64f5a32e3f
Wanthanai <pentorework@gmail.com>
2020-02-09 10:44:30 +0700

    [Start] React

--- Server/config/prod.js
+++ Server/config/prod.js
@@ -1,5 +1,7 @@
 //prod.js - production keys here!!
-export const googleClientID = process.env.GOOGLE_CLIENT_ID;
-export const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET;
-export const mongoURI = process.env.MONGO_URI;
-export const cookieKey = process.env.COOKIE_KEY;
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+};
